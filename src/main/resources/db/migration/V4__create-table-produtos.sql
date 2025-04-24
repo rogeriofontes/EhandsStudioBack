e40fbbe32ed6f3d3abfa794d4f6d6616f704c28e
Amanda Breidenbach <abreidenbach@hotmail.com>
2025-04-24 11:46:20 -0300

    a

--- src/main/resources/db/migration/V4__create-table-produtos.sql
+++ src/main/resources/db/migration/V4__create-table-produtos.sql
@@ -3,7 +3,7 @@ CREATE TABLE produtos (
     nome VARCHAR(255) NOT NULL,
     descricao VARCHAR(255),
     tamanho VARCHAR(255),
-    imagem VARCHAR(255),
-    preco DECIMAL(10, 2) NOT NULL,
-    categoria_id BIGINT,
+    imagem_url VARCHAR(255),
+    preco DECIMAL(10, 2),
+    categoria_id BIGINT
 );
--- src/main/resources/db/migration/V5__create-table-orcamentos.sql
+++ src/main/resources/db/migration/V5__create-table-orcamentos.sql
@@ -1,8 +1,8 @@
 CREATE TABLE orcamentos (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    data_orcamento DATETIME(10),
+    data_orcamento DATETIME(20),
     imagem_url VARCHAR(255),
     status VARCHAR(255),
-    cliente_id BIGINT,
+    clientes_id BIGINT,
     produtos_id BIGINT
 );
