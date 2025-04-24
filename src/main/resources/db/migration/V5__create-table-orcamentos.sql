e9dcb91f70c9561bef56f483cd5b60a72d46f099
Amanda Breidenbach <abreidenbach@hotmail.com>
2025-04-24 11:54:57 -0300

    correcao

--- src/main/resources/db/migration/V5__create-table-orcamentos.sql
+++ src/main/resources/db/migration/V5__create-table-orcamentos.sql
@@ -1,8 +1,8 @@
 CREATE TABLE orcamentos (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    data_orcamento DATETIME(20),
+    data_orcamento DATETIME,
     imagem_url VARCHAR(255),
-    status VARCHAR(255),
+    statuss VARCHAR(255),
     clientes_id BIGINT,
     produtos_id BIGINT
 );
