fc354c3bd410846695e0e65eb4493344133f28b8
Amandabrei <abreidenbach@hotmail.com>
2025-06-17 15:23:06 -0300

    login changes

--- src/main/java/com/maosencantadas/infra/security/TokenService.java
+++ src/main/java/com/maosencantadas/infra/security/TokenService.java
@@ -4,13 +4,16 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.maosencantadas.model.domain.artist.Artist;
 import com.maosencantadas.model.domain.user.User;
+import com.maosencantadas.model.repository.ArtistRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
+import java.util.Optional;
 
 @Service
 public class TokenService {
@@ -19,35 +22,47 @@ public class TokenService {
 
     private String secret;
 
-    public String generateToken(User user){
-        try{
+    private final ArtistRepository artistRepository;
+
+    public TokenService(ArtistRepository artistRepository) {
+        this.artistRepository = artistRepository;
+    }
+
+    public String generateToken(User user) {
+        try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-            String token = JWT.create()
+
+            Optional<Artist> artistOptional = artistRepository.findByUser(user);
+
+            var tokenBuilder = JWT.create()
                     .withIssuer("auth-api")
                     .withSubject(user.getLogin())
-                    .withExpiresAt(genExpirationDate())
-                    .sign(algorithm);
-            return token;
+                    .withClaim("userId", user.getId())
+                    .withClaim("role", user.getRole().name())
+                    .withExpiresAt(genExpirationDate());
+
+            artistOptional.ifPresent(artist -> tokenBuilder.withClaim("artistId", artist.getId()));
+
+            return tokenBuilder.sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Error while generating token", exception);
         }
     }
 
-    public String validateToken(String token){
-        try{
+    public String validateToken(String token) {
+        try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.require(algorithm)
                     .withIssuer("auth-api")
                     .build()
                     .verify(token)
                     .getSubject();
-        }catch (JWTVerificationException exception){
+        } catch (JWTVerificationException exception) {
             return "";
-
         }
     }
 
-    private Instant genExpirationDate(){
+    private Instant genExpirationDate() {
         return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
--- src/main/java/com/maosencantadas/model/repository/ArtistRepository.java
+++ src/main/java/com/maosencantadas/model/repository/ArtistRepository.java
@@ -1,5 +1,6 @@
 package com.maosencantadas.model.repository;
 
+import com.maosencantadas.model.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.maosencantadas.model.domain.artist.Artist;
@@ -13,6 +14,8 @@ public interface ArtistRepository extends JpaRepository<Artist, Long> {
     List<Artist> findByCategoryId(Long categoryId);
 
     Optional<Artist> findByName(String artistName);
+
+    Optional<Artist> findByUser(User user);
 
 
     //List<Artist> findByCategoryName(String categoryName);
